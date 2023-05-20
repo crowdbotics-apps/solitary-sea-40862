be59c211d14cf9ebd7c70944157c6a63813e1b42
Terence Matos <terence.matos@gmail.com>
2023-05-20 14:43:20 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "solitary-sea-40862",
-  url: "https://solitary-sea-40862.botics.co",
-  api: "https://solitary-sea-40862.botics.co/api/v1"
-}
+  "name": "solitary-sea-40862",
+  "url": "https://solitary-sea-40862.botics.co",
+  "api": "https://solitary-sea-40862.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
