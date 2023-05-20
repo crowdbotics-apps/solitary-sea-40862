d572e38687e7c035b39846568c42caa7bc50d492
Crowdbotics <team@crowdbotics.com>
2023-05-20 18:41:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/solitary_sea_40862/MainActivity.java
@@ -0,0 +1,15 @@
+package com.solitary_sea_40862;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "solitary_sea_40862";
+  }
+}
